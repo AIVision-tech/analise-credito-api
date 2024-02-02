3df3d058799e670e26d718379422ed39f3b8e924
Paulo Rebouças <45965959+PauloRMR@users.noreply.github.com>
2024-02-02 00:47:36 -0300

    Desenvolvimento: nova rota de consulta de CPF desenvolvida e base swagger

--- API/main.py
+++ API/main.py
@@ -1,23 +1,38 @@
 from flask import Flask, redirect
 from flasgger import Swagger
+import yaml
+import os
 
-from swagger_config import swagger_config
-from auth_service.auth import create_token
-from protected_service.protected import protected_route
-from sentiment_service.sentiment import analyze
+from services.consulta_cpf.consulta_cpf import consulta_cpf_route
 
 app = Flask(__name__)
-swagger = Swagger(app, config=swagger_config)
 
+# Função para carregar e combinar Swagger docs de múltiplos arquivos YAML
+def load_and_combine_swagger_docs(directory_path):
+    combined_docs = {"paths": {}, "definitions": {}} 
+    for filename in os.listdir(directory_path):
+        if filename.endswith('.yaml'):
+            filepath = os.path.join(directory_path, filename)
+            with open(filepath, 'r') as file:
+                part = yaml.safe_load(file)
+                if "paths" in part:
+                    combined_docs["paths"].update(part["paths"])
+                if "definitions" in part:
+                    combined_docs["definitions"].update(part["definitions"])
+    return combined_docs
+
+# Diretório que contém os arquivos YAML da documentação Swagger
+swagger_docs_dir = os.path.join(os.path.dirname(__file__), 'swagger')
+swagger_template = load_and_combine_swagger_docs(swagger_docs_dir)
+
+swagger = Swagger(app, template=swagger_template)
 
 @app.route('/')
 def index():
     return redirect("/apidocs/")
 
-
-app.add_url_rule('/token', 'create_token', create_token, methods=['POST'])
-app.add_url_rule('/protected', 'protected_route', protected_route, methods=['GET'])
-app.add_url_rule('/sentiment', 'analyze', analyze, methods=['POST'])
+# Atualização das regras de URL para usar as funções de rota corretas
+app.add_url_rule('/consulta_cpf', 'consulta_cpf', consulta_cpf_route, methods=['POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# analise-credito
-repositório de analise de credito, para o projeto da invest build
+# Análise de Crédito
+
+Este é um projeto de análise de crédito que visa avaliar a elegibilidade de indivíduos para empréstimos com base em diversos critérios.
+
+## Licença
+
+Este projeto é licenciado sob a Licença Mozilla. Consulte o arquivo [LICENSE](LICENSE) para obter mais detalhes sobre os termos da licença.
+
+## Status do Projeto
+
+![GitHub last commit](https://img.shields.io/github/last-commit/Paulo-Reboucas-AIVision/analise-credito)
+![GitHub contributors](https://img.shields.io/github/contributors/Paulo-Reboucas-AIVision/analise-credito)
+![GitHub repo size](https://img.shields.io/github/repo-size/Paulo-Reboucas-AIVision/analise-credito)
+
+## Linguagens Utilizadas
+
+![GitHub top language](https://img.shields.io/github/languages/top/Paulo-Reboucas-AIVision/analise-credito)
+
+## Visão Geral
+
+[Inserir uma breve descrição do projeto e seu propósito.]
+
+## Recursos
+
+- Consulta de CPF
+- Consulta de CNPJ
+
+## Como Usar
+
+em construção
+
+## Contribuição
+
+Sinta-se à vontade para contribuir para este projeto. Se você deseja reportar um problema, adicionar um recurso ou fazer melhorias, siga estas etapas:
+1. Fork o projeto
+2. Crie um branch com a sua feature (`git checkout -b feature/sua-feature`)
+3. Commit suas alterações (`git commit -m 'Adicione uma nova feature'`)
+4. Push para o branch (`git push origin feature/sua-feature`)
+5. Abra um pull request
+
+## Contato
+
+aivision-tech.com
+
